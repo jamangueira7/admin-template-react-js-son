b33cf0f4bc62add9ff814486bee1fa3380c212b8
João Mangueira <joao.mangueira@toxicologiapardini.com.br>
2021-01-22 22:59:36 -0300

    Navegação e primeiros components feitos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './styles/app.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+import Home from './components/Home';
+import Login from './components/Login';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route exact path="/Login">
+            <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
