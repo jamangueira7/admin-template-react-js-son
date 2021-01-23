babb8250930fe6869ba51add1ee595772e4a4acf
João Mangueira <joao.mangueira@toxicologiapardini.com.br>
2021-01-23 20:34:56 -0300

    Usando hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import useClass from './../hooks/add-class-body';
 import {
     Container,
     Col,
@@ -12,34 +13,39 @@ import {
     Button,
 } from 'reactstrap';
 
-export default class Login extends Component {
-    render() {
-        return (
-           <div>
-               <Container className="mt-5 flex-row align-items-center">
-                   <Row className="justify-content-center">
-                       <Col md="6">
-                           <Card>
-                             <CardBody>
-                                 <Form>
-                                     <h1>Login</h1>
-                                     <p>Entre com e-mail e senha para acessar</p>
-                                     <InputGroup className="mb-3">
-                                         <Input type="email" placeholder="Email"></Input>
-                                     </InputGroup>
-                                     <InputGroup className="mb-3">
-                                         <Input type="password" placeholder="Senha"></Input>
-                                     </InputGroup>
-                                     <InputGroup>
-                                         <Button color="primary">Go!</Button>
-                                     </InputGroup>
-                                 </Form>
-                             </CardBody>
-                           </Card>
-                       </Col>
-                   </Row>
-               </Container>
-           </div>
-        );
+const Login = () => {
+    useClass('page-login');
+
+    const handleLogin = () => {
+        console.log('login')
     }
+    return (
+       <div>
+           <Container className="mt-5 flex-row align-items-center">
+               <Row className="justify-content-center">
+                   <Col md="6">
+                       <Card>
+                         <CardBody>
+                             <Form>
+                                 <h1>Login</h1>
+                                 <p>Entre com e-mail e senha para acessar</p>
+                                 <InputGroup className="mb-3">
+                                     <Input type="email" placeholder="Email"></Input>
+                                 </InputGroup>
+                                 <InputGroup className="mb-3">
+                                     <Input type="password" placeholder="Senha"></Input>
+                                 </InputGroup>
+                                 <InputGroup>
+                                     <Button color="primary" onClick={handleLogin}>Go!</Button>
+                                 </InputGroup>
+                             </Form>
+                         </CardBody>
+                       </Card>
+                   </Col>
+               </Row>
+           </Container>
+       </div>
+    );
 }
+
+export default Login;
