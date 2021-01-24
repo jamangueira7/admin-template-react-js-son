1937f909f559046c926deba971a8ef314e9445f3
João Mangueira <jamjs7@gmail.com>
2021-01-24 15:51:40 -0300

    finalizando sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,24 @@ import React from 'react';
 import {
     Navbar,
     NavbarBrand,
-    NavbarToggler
+    NavbarToggler,
+    Nav
 } from 'reactstrap';
 
 export default function Header(props) {
+    function handleClickToSidebar (e) {
+        e.preventDefault();
+        document.getElementById('main-wrapper').classList.toggle('toggled');
+    }
     return (
         <header>
             <Navbar color="light" light>
                 <NavbarBrand>
                     <img src="https://via.placeholder.com/150x50" alt="" />
                 </NavbarBrand>
-                <NavbarToggler/>
+                <Nav className="mr-auto" navbar>
+                    <NavbarToggler onClick={handleClickToSidebar}/>
+                </Nav>
             </Navbar>
         </header>
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Templates from './../containers/routes';
 
 export default function Sidebar(props) {
+    useEffect(() => {
+        document.getElementById('main-wrapper').classList.toggle('toggled');
+        return () => {
+            document.getElementById('main-wrapper').classList.remove('toggled');
+        }
+    },[]);
+
     return (
         <aside id="sidebar">
             <div className="sidebar-title">
